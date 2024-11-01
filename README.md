5c9e1079a34ea6b1291ab83b7dbceb63ec60d054
ArbuesEnriquePerezVillegas <arbues.perez.v@uni.pe>
2024-11-01 16:46:50 -0500

    Se agrego cosas ramdom

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # prueba3
-enseñandsa
+
 holaaaaa
+
+jfieaihfe
+
+## gaa
+
---
+++ prueba.py
@@ -0,0 +1,7 @@
+import random
+import time
+i=0
+while i<10:
+    print(random.randint(1,10))
+    time.sleep(1)
+    i+=1
